633aeb5030e94751c61901d62d54d0a8bc72b2e8
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2024-11-04 08:43:09 +0100

    Added some basic functionality

---
+++ src/Infrastructure/Http/HttpStatusCode.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Http;
+
+enum HttpStatusCode: int
+{
+    case NOT_FOUND = 404;
+    case UNAUTHORIZED = 401;
+    case BAD_REQUEST = 400;
+    case INTERNAL_SERVER_ERROR = 500;
+}
